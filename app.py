1ad879fcb7cf400a04d88ad5840be13eb0c3a240
anarkisoft <anarkisoft@gmail.com>
2024-03-20 16:49:09 -0500

    secon commit

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ app = Flask(__name__)
 # Obtén el puerto desde la variable de entorno PORT, o usa 8080 por defecto
 port = 8080
 
+
+# Nueva ruta para obtener la lista de usuarios falsos
+@app.route('/usuarios')
+def obtener_usuarios():
+    # Lista de usuarios falsos
+    usuarios_falsos = [
+        {"id": 1, "nombre": "Juan", "edad": 25},
+        {"id": 2, "nombre": "María", "edad": 30},
+        {"id": 3, "nombre": "Pedro", "edad": 28}
+    ]
+    return jsonify(usuarios_falsos)
+
+
 @app.route('/')
 def hello():
     return '¡Hola Mundo!'
